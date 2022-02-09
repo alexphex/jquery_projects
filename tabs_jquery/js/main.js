cc2fb61912027440af9d1cba6012c3c94ae301a9
alexphex <alexv2019@inbox.ru>
2022-02-09 18:49:59 +0200

    tabs on jQuery
    example for a block where you need tabs , minimum css/html/jquery

---
+++ tabs_jquery/js/main.js
@@ -0,0 +1,10 @@
+
+$(function() {
+  
+    $('ul.tabs__name').on('click', 'li:not(.active)', function() {
+      $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+    
+  });
